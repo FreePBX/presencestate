96cf5f0adce7e584d52ee4bef38400b831cc15f0
Jason Parker <jason.parker@schmoozecom.com>
2014-02-20 12:30:20 -0600

    Add setting of preferences.

--- functions.inc.php
+++ functions.inc.php
@@ -19,6 +19,45 @@ function presencestate_display_get($state) {
 	return isset($display[$state]) ? $display[$state] : NULL;
 }
 
+function presencestate_prefs_get($extension) {
+	global $db;
+
+	$sql = 'SELECT item_id, pref FROM presencestate_prefs WHERE extension = ?';
+	$ret = $db->getAll($sql, array($extension), DB_FETCHMODE_ASSOC);
+
+	if (DB::isError($ret)) {
+		die_freepbx("Could not get presence state preferences.\n");
+	}
+
+	$presencestates = presencestate_list_get();
+
+	foreach ($presencestates as $presencestate) {
+		if ($presencestate['type'] == "dnd") {
+			/* Default to DND. */
+			$prefs[$presencestate['id']] = "dnd";
+		} else {
+			$prefs[$presencestate['id']] = '';
+		}
+	}
+
+	foreach ($ret as $row) {
+		$prefs[$row['item_id']] = $row['pref'];
+	}
+
+	return $prefs;
+}
+
+function presencestate_prefs_set($extension, $vars) {
+	global $db;
+
+	$sql = 'REPLACE INTO presencestate_prefs (extension, item_id, pref) VALUES (?, ?, ?)';
+	$ret = $db->query($sql, array($extension, $vars['id'], $vars['pref']));
+
+	if (DB::isError($ret)) {
+		die_freepbx("Could not save presence state preference.\n");
+	}
+}
+
 function presencestate_list_get() {
 	global $db;
 
@@ -37,7 +76,7 @@ function presencestate_list_get() {
 	return $presencestates;
 }
 
-function presencestate_item_put($vars) {
+function presencestate_item_set($vars) {
 	global $db;
 
 	$sql = 'REPLACE INTO presencestate_list (id, type, message) VALUES (?, ?, ?)';
@@ -56,6 +95,13 @@ function presencestate_item_put($vars) {
 
 function presencestate_item_del($id) {
 	global $db;
+
+	$sql = 'DELETE FROM presencestate_prefs WHERE `item_id` = ?';
+	$ret = $db->query($sql, array($id));
+
+	if (DB::isError($ret)) {
+		die_freepbx("Could not delete presence state.\n");
+	}
 
 	$sql = 'DELETE FROM presencestate_list WHERE `id` = ?';
 	$ret = $db->query($sql, array($id));
