d8033ecbdd16fb47b9b12de7ff032962d09d1b9c
James Finstrom <jfinstrom@sangoma.com>
2018-07-10 22:30:05 +0000

    fix namespace

--- Backup.php
+++ Backup.php
@@ -1,9 +1,8 @@
 <?php
-namespace FreePBX\modules\__MODULENAME__;
+namespace FreePBX\modules\Presencestate;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
   public function runBackup($id,$transaction){
-    $this->addDependency('');
     $configs = [
         'states' => $this->FreePBX->Presencestate->getAllStates(),
         'prefs' => $this->FreePBX->Presencestate->dumpPrefs(),
