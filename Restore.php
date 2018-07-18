72f935ed2d1d8ab3936f2bdd849d603e88d38dab
James Finstrom <jfinstrom@sangoma.com>
2018-07-18 22:19:45 +0000

    Cleanup restore

--- Restore.php
+++ Restore.php
@@ -3,8 +3,8 @@ namespace FreePBX\modules\Presencestate;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
-      $configs = $this->getConfigs();
-      foreach($configs['statess'] as $state){
+      $configs = reset($this->getConfigs());
+      foreach($configs['states'] as $state){
           $this->FreePBX->Presencestate->setItem($state);
       }
       $this->FreePBX->Presencestate->loadPrefs($configs['prefs']);
