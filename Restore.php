b2aca6275502cb1be1efccbeae664c350e12d593
Andrew Nagy <andrew.nagy@the159.com>
2019-02-11 13:48:06 -0500

    backup and restore

--- Restore.php
+++ Restore.php
@@ -3,7 +3,7 @@ namespace FreePBX\modules\Presencestate;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
-      $configs = reset($this->getConfigs());
+      $configs = $this->getConfigs();
       foreach($configs['states'] as $state){
           $this->FreePBX->Presencestate->setItem($state);
       }
