a6158c11148530b6490fec962561fac3621104d9
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 19:28:45 -0500

    [Module Tag script: presencestate 15.0.5] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -2,31 +2,14 @@
 namespace FreePBX\modules\Presencestate;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-      $configs = $this->getConfigs();
-      foreach($configs['states'] as $state){
-          $this->FreePBX->Presencestate->setItem($state);
-      }
-      $this->FreePBX->Presencestate->loadPrefs($configs['prefs']);
-  }
-    public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir)
-    {
-        $tables = array_flip($tables + $unknownTables);
-        if (!isset($tables['presencestate_list'])) {
-            return $this;
-        }
-        $cb = $this->FreePBX->Presencestate;
-        $cb->setDatabase($pdo);
-        $configs = [
-            'states' => $this->FreePBX->Presencestate->getAllStates(),
-            'prefs' => $this->FreePBX->Presencestate->dumpPrefs(),
-        ];
-        $cb->resetDatabase();
-        foreach ($configs['states'] as $state) {
-           $cb->setItem($state);
-        }
-        $cb->loadPrefs($configs['prefs']);
-
-        return $this;
-    }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		foreach($configs['states'] as $state){
+			$this->FreePBX->Presencestate->setItem($state);
+		}
+		$this->FreePBX->Presencestate->loadPrefs($configs['prefs']);
+	}
+	public function processLegacy($pdo, $data, $tables, $unknownTables) {
+		$this->restoreLegacyDatabase($pdo);
+	}
 }
